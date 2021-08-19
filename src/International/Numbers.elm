c38b1161e1a4001adf3410b2ff7b0187c1e77816
Agustín Zubiaga <sup@aguz.me>
2021-08-18 21:15:43 -0300

    ✨ Integer Padding

--- src/International/Numbers.elm
+++ src/International/Numbers.elm
@@ -1,38 +1,100 @@
-module International.Numbers exposing (format, latnInt)
+module International.Numbers exposing
+    ( Format
+    , Number
+    , NumberingSystem(..)
+    , Rounding(..)
+    , Sorrounding(..)
+    , Symbols
+    , format
+    )
 
 import Dict exposing (Dict)
 
 
-type Number
-    = Number
-        { system : NumberingSystem
-        , value : Int
-        }
+type alias Number =
+    { system : NumberingSystem
+    , format : Format
+    , symbols : Symbols
+    , value : Int
+    }
+
+
+type NumberingSystem
+    = Numeric String
+
+
+type alias Symbols =
+    { decimal : String
+    , group : String
+    , list : String
+    , percentSign : String
+    , plusSign : String
+    , minusSign : String
+    , approximatelySign : String
+    , exponential : String
+    , superscriptingExponent : String
+    , perMille : String
+    , infinity : String
+    , nan : String
+    , timeSeparator : String
+    }
+
+
+type Sorrounding
+    = PlusSymbol
+    | MinusSymbol
+    | Literal String
+
+
+type Rounding
+    = Floor
+    | Round
+    | Ceiling
+
+
+type alias Format =
+    { integerPadding : Int
+    , secondaryGroupingSize : Int
+    , primaryGroupingSize : Int
+    , minDecimalPlaces : Int
+    , maxDecimalPlaces : Int
+    , rounding : Rounding
+    , positiveSorrounding : ( List Sorrounding, List Sorrounding )
+    , negativeSorrounding : Maybe ( List Sorrounding, List Sorrounding )
+    }
 
 
 format : Number -> String
-format (Number { system, value }) =
+format num =
     let
         (Numeric digits) =
-            system
+            num.system
+
+        intPart : String
+        intPart =
+            numericToString digits num.value
+
+        paddingCount : Int
+        paddingCount =
+            num.format.integerPadding - String.length intPart
     in
-    numericToString digits value
+    String.repeat paddingCount (getDigit 0 digits) ++ intPart
 
 
 type alias Digits =
     Dict Int String
 
 
-numericToString : Digits -> Int -> String
+numericToString : String -> Int -> String
 numericToString digits value =
     if value >= 0 then
         positiveNumericToString digits value ""
 
     else
-        "-" ++ positiveNumericToString digits (value * -1) ""
+        "-" ++ positiveNumericToString digits (abs value) ""
 
 
-positiveNumericToString : Digits -> Int -> String -> String
+positiveNumericToString : String -> Int -> String -> String
 positiveNumericToString digits value suffix =
     if value < 10 then
         getDigit value digits ++ suffix
@@ -44,32 +106,6 @@ positiveNumericToString digits value suffix =
             (getDigit (remainderBy 10 value) digits ++ suffix)
 
 
-getDigit : Int -> Digits -> String
-getDigit value digits =
-    Dict.get value digits |> Maybe.withDefault ""
-
-
-latnInt : Int -> Number
-latnInt value =
-    Number { system = latn, value = value }
-
-
-type NumberingSystem
-    = Numeric Digits
-
-
-latn : NumberingSystem
-latn =
-    Numeric <|
-        Dict.fromList
-            [ ( 0, "0" )
-            , ( 1, "1" )
-            , ( 2, "2" )
-            , ( 3, "3" )
-            , ( 4, "4" )
-            , ( 5, "5" )
-            , ( 6, "6" )
-            , ( 7, "7" )
-            , ( 8, "8" )
-            , ( 9, "9" )
-            ]
+getDigit : Int -> String -> String
+getDigit index =
+    String.slice index (index + 1)
--- tests/NumberTests.elm
+++ tests/NumberTests.elm
@@ -11,7 +11,74 @@ suite =
     describe "Numbers"
         [ fuzz Fuzz.int "formats ints with numeric systems" <|
             \value ->
-                Num.latnInt value
-                    |> Num.format
+                value
+                    |> formatWith defaultFormat
                     |> Expect.equal (String.fromInt value)
+        , describe "integer padding"
+            [ test "adds missing zeroes" <|
+                \_ ->
+                    42
+                        |> formatWith { defaultFormat | integerPadding = 6 }
+                        |> Expect.equal "000042"
+            , test "does not adds any zeros if length matches" <|
+                \_ ->
+                    4200
+                        |> formatWith { defaultFormat | integerPadding = 4 }
+                        |> Expect.equal "4200"
+            , test "uses numbering systems digit for 0 instead of hardcoded '0'" <|
+                \_ ->
+                    { system = Num.Numeric "Z123456789"
+                    , format = { defaultFormat | integerPadding = 3 }
+                    , symbols = symbols
+                    , value = 1
+                    }
+                        |> Num.format
+                        |> Expect.equal "ZZ1"
+            ]
         ]
+
+
+formatWith : Num.Format -> Int -> String
+formatWith format value =
+    Num.format
+        { system = latn
+        , format = format
+        , symbols = symbols
+        , value = value
+        }
+
+
+latn : Num.NumberingSystem
+latn =
+    Num.Numeric "0123456789"
+
+
+defaultFormat : Num.Format
+defaultFormat =
+    { integerPadding = 0
+    , secondaryGroupingSize = 3
+    , primaryGroupingSize = 3
+    , minDecimalPlaces = 0
+    , maxDecimalPlaces = 5
+    , rounding = Num.Round
+    , positiveSorrounding = ( [], [] )
+    , negativeSorrounding = Nothing
+    }
+
+
+symbols : Num.Symbols
+symbols =
+    { decimal = "."
+    , group = ","
+    , list = ";"
+    , percentSign = "%"
+    , plusSign = "+"
+    , minusSign = "-"
+    , approximatelySign = "~"
+    , exponential = "E"
+    , superscriptingExponent = "×"
+    , perMille = "‰"
+    , infinity = "∞"
+    , nan = "NaN"
+    , timeSeparator = ":"
+    }
