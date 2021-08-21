aa4174648d8b5e7d6fc3b84f06dac311830af7f7
Agustín Zubiaga <sup@aguz.me>
2021-08-21 19:33:15 -0300

    ✨ Integer Grouping and opaque types

--- src/International/Numbers.elm
+++ src/International/Numbers.elm
@@ -2,21 +2,32 @@ module International.Numbers exposing
     ( Format
     , Number
     , NumberingSystem(..)
-    , Rounding(..)
-    , Sorrounding(..)
     , Symbols
-    , format
+    , createFormat
+    , groupEach
+    , groupWith
+    , int
+    , padLeft
+    , toString
     )
 
 import Dict exposing (Dict)
+import String.Extra as String
 
 
-type alias Number =
-    { system : NumberingSystem
-    , format : Format
-    , symbols : Symbols
-    , value : Int
-    }
+type Grouping
+    = DoNotGroup
+    | GroupEach Int
+    | GroupWith Int Int
+
+
+type Format
+    = Format
+        { system : NumberingSystem
+        , symbols : Symbols
+        , intPadding : Int
+        , intGrouping : Grouping
+        }
 
 
 type NumberingSystem
@@ -40,67 +51,100 @@ type alias Symbols =
     }
 
 
-type Sorrounding
-    = PlusSymbol
-    | MinusSymbol
-    | Literal String
+createFormat : Symbols -> NumberingSystem -> Format
+createFormat symbols system =
+    Format
+        { system = system
+        , symbols = symbols
+        , intPadding = 0
+        , intGrouping = DoNotGroup
+        }
 
 
-type Rounding
-    = Floor
-    | Round
-    | Ceiling
+padLeft : Int -> Format -> Format
+padLeft padding (Format f) =
+    Format { f | intPadding = padding }
 
 
-type alias Format =
-    { integerPadding : Int
-    , secondaryGroupingSize : Int
-    , primaryGroupingSize : Int
-    , minDecimalPlaces : Int
-    , maxDecimalPlaces : Int
-    , rounding : Rounding
-    , positiveSorrounding : ( List Sorrounding, List Sorrounding )
-    , negativeSorrounding : Maybe ( List Sorrounding, List Sorrounding )
-    }
+groupEach : Int -> Format -> Format
+groupEach size (Format f) =
+    Format { f | intGrouping = GroupEach size }
 
 
-format : Number -> String
-format num =
+groupWith : Int -> Int -> Format -> Format
+groupWith mostSignificant leastSignificant (Format f) =
+    Format { f | intGrouping = GroupWith mostSignificant leastSignificant }
+
+
+type Number
+    = Number Int Format
+
+
+int : Int -> Format -> Number
+int =
+    Number
+
+
+toString : Number -> String
+toString (Number value wrappedFormat) =
     let
+        (Format { symbols, system, intPadding, intGrouping }) =
+            wrappedFormat
+
         (Numeric digits) =
-            num.system
+            system
 
-        intPart : String
-        intPart =
-            numericToString digits num.value
+        str : String
+        str =
+            numericToString digits value ""
 
-        paddingCount : Int
-        paddingCount =
-            num.format.integerPadding - String.length intPart
+        padCount : Int
+        padCount =
+            intPadding - String.length str
+
+        padded : String
+        padded =
+            String.repeat padCount (getDigit 0 digits) ++ str
     in
-    String.repeat paddingCount (getDigit 0 digits) ++ intPart
+    case intGrouping of
+        GroupEach size ->
+            groupHelp size symbols.group padded ""
+
+        GroupWith mostSignificant leastSignificant ->
+            if String.length padded > leastSignificant then
+                groupHelp mostSignificant symbols.group (String.dropRight leastSignificant padded) ""
+                    ++ symbols.group
+                    ++ String.right leastSignificant padded
+
+            else
+                groupHelp leastSignificant symbols.group padded ""
+
+        DoNotGroup ->
+            padded
 
 
-type alias Digits =
-    Dict Int String
-
-
-numericToString : String -> Int -> String
-numericToString digits value =
-    if value >= 0 then
-        positiveNumericToString digits value ""
+groupHelp : Int -> String -> String -> String -> String
+groupHelp groupSize symbol pending done =
+    if String.length pending > groupSize && groupSize > 0 then
+        groupHelp groupSize
+            symbol
+            (String.dropRight groupSize pending)
+            (symbol
+                ++ String.right groupSize pending
+                ++ done
+            )
 
     else
-        "-" ++ positiveNumericToString digits (abs value) ""
+        pending ++ done
 
 
-positiveNumericToString : String -> Int -> String -> String
-positiveNumericToString digits value suffix =
+numericToString : String -> Int -> String -> String
+numericToString digits value suffix =
     if value < 10 then
         getDigit value digits ++ suffix
 
     else
-        positiveNumericToString
+        numericToString
             digits
             (value // 10)
             (getDigit (remainderBy 10 value) digits ++ suffix)
--- tests/NumberTests.elm
+++ tests/NumberTests.elm
@@ -1,72 +1,110 @@
 module NumberTests exposing (suite)
 
 import Expect
-import Fuzz
-import International.Numbers as Num
-import Test exposing (Test, describe, fuzz, test)
+import International.Numbers as Numbers
+import Test exposing (Test, describe, only, test)
 
 
 suite : Test
 suite =
     describe "Numbers"
-        [ fuzz Fuzz.int "formats ints with numeric systems" <|
-            \value ->
-                value
-                    |> formatWith defaultFormat
-                    |> Expect.equal (String.fromInt value)
-        , describe "integer padding"
+        [ describe "integer padding"
             [ test "adds missing zeroes" <|
                 \_ ->
-                    42
-                        |> formatWith { defaultFormat | integerPadding = 6 }
-                        |> Expect.equal "000042"
+                    format
+                        |> Numbers.padLeft 6
+                        |> Numbers.int 42
+                        |> expectFormat "000042"
             , test "does not adds any zeros if length matches" <|
                 \_ ->
-                    4200
-                        |> formatWith { defaultFormat | integerPadding = 4 }
-                        |> Expect.equal "4200"
+                    format
+                        |> Numbers.padLeft 4
+                        |> Numbers.int 4200
+                        |> expectFormat "4200"
             , test "uses numbering systems digit for 0 instead of hardcoded '0'" <|
                 \_ ->
-                    { system = Num.Numeric "Z123456789"
-                    , format = { defaultFormat | integerPadding = 3 }
-                    , symbols = symbols
-                    , value = 1
-                    }
-                        |> Num.format
+                    Numbers.createFormat symbols (Numbers.Numeric "Z123456789")
+                        |> Numbers.padLeft 3
+                        |> Numbers.int 1
+                        |> Numbers.toString
                         |> Expect.equal "ZZ1"
+            ]
+        , describe "integer grouping"
+            [ test "does not group if length < group size" <|
+                \_ ->
+                    format
+                        |> Numbers.groupEach 3
+                        |> Numbers.int 123
+                        |> expectFormat "123"
+            , test "groups when length > group size" <|
+                \_ ->
+                    format
+                        |> Numbers.groupEach 3
+                        |> Numbers.int 1234
+                        |> expectFormat "1,234"
+            , test "groups multiple times" <|
+                \_ ->
+                    format
+                        |> Numbers.groupEach 3
+                        |> Numbers.int 1234567
+                        |> expectFormat "1,234,567"
+            , test "supports arbitrary sizes" <|
+                \_ ->
+                    format
+                        |> Numbers.groupEach 2
+                        |> Numbers.int 12345
+                        |> expectFormat "1,23,45"
+            , test "groups after integer padding" <|
+                \_ ->
+                    format
+                        |> Numbers.padLeft 9
+                        |> Numbers.groupEach 3
+                        |> Numbers.int 1234
+                        |> expectFormat "000,001,234"
+            , test "uses specified grouping symbol" <|
+                \_ ->
+                    Numbers.createFormat { symbols | group = "." } latn
+                        |> Numbers.groupEach 3
+                        |> Numbers.int 1234
+                        |> expectFormat "1.234"
+            , test "different primary / secondary group sizes" <|
+                \_ ->
+                    format
+                        |> Numbers.groupWith 2 3
+                        |> Numbers.int 123456789
+                        |> expectFormat "12,34,56,789"
+            , test "does not group if length <= primary group size" <|
+                \_ ->
+                    format
+                        |> Numbers.groupWith 2 3
+                        |> Numbers.int 123
+                        |> expectFormat "123"
+            , test "does not group if size < 1" <|
+                \_ ->
+                    format
+                        |> Numbers.groupEach 0
+                        |> Numbers.int 1234567
+                        |> expectFormat "1234567"
             ]
         ]
 
 
-formatWith : Num.Format -> Int -> String
-formatWith format value =
-    Num.format
-        { system = latn
-        , format = format
-        , symbols = symbols
-        , value = value
-        }
+expectFormat : String -> Numbers.Number -> Expect.Expectation
+expectFormat expected =
+    Numbers.toString >> Expect.equal expected
 
 
-latn : Num.NumberingSystem
+format : Numbers.Format
+format =
+    Numbers.createFormat symbols latn
+
+
+latn : Numbers.NumberingSystem
 latn =
-    Num.Numeric "0123456789"
+    Numbers.Numeric "0123456789"
 
 
-defaultFormat : Num.Format
-defaultFormat =
-    { integerPadding = 0
-    , secondaryGroupingSize = 3
-    , primaryGroupingSize = 3
-    , minDecimalPlaces = 0
-    , maxDecimalPlaces = 5
-    , rounding = Num.Round
-    , positiveSorrounding = ( [], [] )
-    , negativeSorrounding = Nothing
-    }
-
-
-symbols : Num.Symbols
+symbols : Numbers.Symbols
 symbols =
     { decimal = "."
     , group = ","
